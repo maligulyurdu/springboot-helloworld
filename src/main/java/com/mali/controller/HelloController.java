5ddf22842b7b4e777c5ac3db3ade855aefc3bdc6
maligulyurdu <maligulyurdu@gmail.com>
2024-07-04 15:00:20 +0300

    mapping annotations

--- src/main/java/com/mali/controller/HelloController.java
+++ src/main/java/com/mali/controller/HelloController.java
@@ -1,16 +1,37 @@
 package com.mali.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping(path = "/api")
 public class HelloController {
 
-    //localhost:8080/hello
+    //localhost:8080/api/hello
 
+    // GET: When we want to see the data. @GetMapping
+    // POST: When we want to save the data. @PostMapping
+    // PUT: When we want to update the data. @PutMapping
+    // PATCH: When we want to update the part of the data. @PatchMapping
+    //       for example: want update the password.
+    // DELETE: When we want to delete the data. @DeleteMapping
+
+
+    // @RequestMapping(path = "/hello", method = RequestMethod.GET) -> Same with bottom line
     @GetMapping(path = "/hello")
     public String sayHello(){
         return "Hello World!";
     }
+
+    @PostMapping("/save") // You can also use path = "/hello"
+    public String save(){
+        return "Data saved!";
+    }
+
+    @DeleteMapping("/delete")
+    public String delete(){
+        return "Data deleted!";
+    }
+
+
 }
 
